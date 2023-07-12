65c7871840a4abc82780e629b695fdfadcb7cd01
Adetoba Adeleke <eadeleke201@gmail.com>
2023-07-12 14:56:01 +0100

    correction for 0-create_array task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,20 +15,19 @@
 char *create_array(unsigned int size, char c)
 
 {
-	char *str;
-
 	unsigned int i;
+	char *s;
+	s = NULL;
 
-
-	str = malloc(sizeof(char) * size);
-	if
-		(size == 0 || str == NULL)
-	return (NULL);
-	for
-		(i = 0; i < size;);
-	i++;
-	str[i] = c;
-
-	return (str);
+	if (s == 0)
+		return (NULL);
+	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
 
 }
